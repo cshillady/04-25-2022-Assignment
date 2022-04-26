a0bddb4bb8b14bc5a41fb7c3c02dca8142ee9464
Connor Shillady <c.shillady24@gmail.com>
2022-04-25 20:21:26 -0400

    push via clone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a test project.");
+        System.out.println("This is a test update from cloned repo.");
     }
 }
